efb9772d479a84625c559f4624b30b5efb5833e6
varun4747 <varunpapanaboina4747@gmail.com>
2025-07-18 14:39:05 +0530

    FormData_variables

---
+++ FormData.java
@@ -0,0 +1,103 @@
+public class FormData {
+
+    public static void main(String[] args) {
+        char isStudentfirstyear='F';
+        char isStudentTransferee='T';
+        char isStudentReturnee='R';
+        String LastName="warner";
+        String FirstName="tim";
+        String MiddleName="david";
+        String Address="xyz village";
+        String DateOfBirth="11-11-0000";
+        String BirthPlace="Us";
+        char isCitizenship_Filipino='Y';
+        char isCitizenship_Others='O';
+        boolean isStudentMale=true;
+        boolean isStudentFemale=false;
+        char isCivilStatus_Single='S';
+        char isCivilStatus_Married='M';
+        String isCivilStatus_Others="***";
+        long contactNumber =7416467669l;
+        String Religion="Hindu";
+        String NameOfTheSpouse="****";
+        long contactNumberOfSpouse=9876543212l;
+        String NameOfFather="Jhon";
+        String OccupationOfFather="dancer";
+        long contactNumberOfFather=7687675767l;
+        String GuardiansName="no";
+        String GuardiansAddress="not applicable";
+        long contactNumberOfGuardians=0;
+        String NameOfMother="thana";
+        String OccupationOfMother="teacher";
+        long contactNumberOfMother=7687675767l;
+        String SeniorHighSchool="palmsprings";
+        String SHSstrand="12th";
+        short YearGraduated=2025;
+        byte GenAverage=85;
+        short LastSchoolAttended=12-12-2025;
+        String Course="IT";
+        short LastSchoolYear=2025;
+        boolean isALSpasser_Yes=true;
+        boolean isALSpasser_No=false;
+        boolean isFamily4PsBeneficiary_Yes=true;
+        boolean isFamily4PsBeneficiary_No=false;
+        boolean ifDisabilityORpwd_yes=true;
+        boolean ifDisabilityORpwd_No=false;
+        short YearTaken=2025;
+        byte Average=90;
+        long FourPsIDNumber=8754526352l;
+        String TypeOfDisability="no";
+
+
+        System.out.println(isStudentfirstyear);
+        System.out.println(isStudentTransferee);
+        System.out.println(isStudentReturnee);
+        System.out.println(LastName);
+        System.out.println(FirstName);
+        System.out.println(MiddleName);
+        System.out.println(Address);
+        System.out.println(DateOfBirth);
+        System.out.println( BirthPlace);
+        System.out.println(isCitizenship_Filipino);
+        System.out.println(isCitizenship_Others);
+        System.out.println(isStudentMale);
+        System.out.println(isStudentFemale);
+        System.out.println(isCivilStatus_Single);
+        System.out.println(isCivilStatus_Married);
+        System.out.println(isCivilStatus_Others);
+        System.out.println(contactNumber);
+        System.out.println(Religion);
+        System.out.println(NameOfTheSpouse);
+        System.out.println(contactNumberOfSpouse);
+        System.out.println(NameOfFather);
+        System.out.println(OccupationOfFather);
+        System.out.println(contactNumberOfFather);
+        System.out.println(GuardiansName);
+        System.out.println(GuardiansAddress);
+        System.out.println(contactNumberOfGuardians);
+        System.out.println(NameOfMother);
+        System.out.println(OccupationOfMother);
+        System.out.println(contactNumberOfMother);
+        System.out.println(SeniorHighSchool);
+        System.out.println(SHSstrand);
+        System.out.println(YearGraduated);
+        System.out.println(GenAverage);
+        System.out.println(LastSchoolAttended);
+        System.out.println(Course);
+        System.out.println(LastSchoolYear);
+        System.out.println(isALSpasser_Yes);
+        System.out.println(isALSpasser_No);
+        System.out.println(isFamily4PsBeneficiary_Yes);
+        System.out.println(isFamily4PsBeneficiary_No);
+        System.out.println(ifDisabilityORpwd_yes);
+        System.out.println(ifDisabilityORpwd_No);
+        System.out.println(YearTaken);
+        System.out.println(Average);
+        System.out.println(FourPsIDNumber);
+        System.out.println(TypeOfDisability);
+        
+
+
+    }
+    
+}
